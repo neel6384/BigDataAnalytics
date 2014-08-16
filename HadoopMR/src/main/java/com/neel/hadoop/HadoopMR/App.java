fc09844409f775144816e59199c9de4d6aaf0037
neel <neel@mydellpc>
2014-08-16 22:22:34 +0530

    SAmple commit

---
+++ HadoopMR/src/main/java/com/neel/hadoop/HadoopMR/App.java
@@ -0,0 +1,13 @@
+package com.neel.hadoop.HadoopMR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
